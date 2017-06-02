41078c417c3731fb9b3e119f582dd26c0766c774
mmJava <maldojavadev@gmail.com>
2017-06-02 17:39:07 -0600

    update readme and added src file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is my week 1 homework project
 
 I am learning Java expressions
+
+I need to make comments on my work here!
---
+++ src/MakeChange2/MakeChangeClass.java
@@ -0,0 +1,12 @@
+package MakeChange2;
+
+import java.util.Scanner;
+
+public class MakeChangeClass {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+	}
+
+}
